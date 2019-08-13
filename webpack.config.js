d3c3b8b6c4107d64048598c4d316b95afd4fce8a
Alex Bravo <49041010+a-bravo@users.noreply.github.com>
2019-08-12 21:32:42 -0700

    Add sass

--- webpack.config.js
+++ webpack.config.js
@@ -15,6 +15,7 @@ module.exports = {
       { test: /\.js$/, use: 'babel-loader' },
       { test: /\.vue$/, use: 'vue-loader' },
       { test: /\.css$/, use: ['vue-style-loader', 'css-loader'] },
+      { test: /\.scss$/, use: ['vue-style-loader', 'css-loader', 'sass-loader'] },
     ],
   },
   plugins: [
