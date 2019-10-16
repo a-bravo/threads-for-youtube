0142d79e8c3672ec2f68ed031af1ef39b7481193
Alex Bravo <49041010+a-bravo@users.noreply.github.com>
2019-10-16 13:00:52 -0700

    Add tests for authorStatusMixin

---
+++ tests/authorStatusMixin.test.js
@@ -0,0 +1,41 @@
+import authorStatusMixin from '../src/mixins/authorStatusMixin';
+import { YT_LINK_CLASS } from '../src/constants';
+
+
+describe('authorStatusMixin', () => {
+  describe('authorClass', () => {
+    test.each([
+      [undefined, undefined, YT_LINK_CLASS],
+      [undefined, true, 'op'],
+      [null, true, 'op'],
+      ['moderator', undefined, 'moderator'],
+      ['moderator', true, 'moderator'],
+      ['admin', undefined, 'admin'],
+      ['special', true, 'special'],
+    ])(
+      'when distinguished: %s, isSubmitter: %s',
+      (a, b, expected) => {
+        expect(authorStatusMixin.methods.authorClass(a, b)[expected]).toBe(true);
+      },
+    );
+  });
+
+  describe('getAuthorStatus', () => {
+    test.each([
+      [null, true, '[S]'],
+      ['moderator', true, '[S,M]'],
+      ['moderator', false, '[M]'],
+      ['admin', false, '[A]'],
+      ['admin', undefined, '[A]'],
+      ['admin', true, '[S,A]'],
+      ['special', false, '[Δ]'],
+      ['special', true, '[S,Δ]'],
+      ['other', true, '[S]'],
+    ])(
+      'when distinguished: %s, isSubmitter: %s',
+      (a, b, expected) => {
+        expect(authorStatusMixin.methods.getAuthorStatus(a, b)).toBe(expected);
+      },
+    );
+  });
+});
